9560053de4d76776cd39f3d9aefddf31e1222d34
scardona14 <cardanasean123@gmail.com>
2024-09-18 13:34:47 -0400

    Choropleth map

---
+++ Visualize_data_with_Choropleth_Map/tooltip.js
@@ -0,0 +1,11 @@
+
+2. My tooltip should have a "data-education" property that corresponds to the "data-education" of the active area.
+Tooltip's "data-education" property should be equal to the active area's "data-education" property: expected '30.1' to equal '20.4'
+AssertionError: Tooltip's "data-education" property should be equal to the active area's "data-education" property: expected '30.1' to equal '20.4'
+    at n.equal (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:556:320)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:581:117575
+    at p (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:581:94539)
+    at Generator.<anonymous> (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:581:95876)
+    at Generator.next (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:581:94902)
+    at r (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:1:1169)
+    at s (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:1:1380)
